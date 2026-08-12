374cc7a82a07b6db1c74c9fa610cee4fd44c7bc5
elink <msh_372@yahoo.com>
2026-08-12 10:09:10 -0500

    Fix navigation clicks for Jenkins test execution

--- src/test/java/com/sam/automation/pages/HomePage.java
+++ src/test/java/com/sam/automation/pages/HomePage.java
@@ -2,7 +2,9 @@ package com.sam.automation.pages;
 
 import com.sam.automation.utils.WaitUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
@@ -12,24 +14,56 @@ public class HomePage {
     private final By loginLink = By.id("login2");
     private final By signUpLink = By.id("signin2");
 
-    private final By samsungGalaxyS6 = By.xpath("//a[text()='Samsung galaxy s6']");
+    private final By samsungGalaxyS6 =
+            By.xpath("//a[text()='Samsung galaxy s6']");
 
-    public HomePage(WebDriver driver){
+    public HomePage(WebDriver driver) {
         this.driver = driver;
         this.waitUtils = new WaitUtils(driver);
     }
-    public LoginPage clickLogin(){
-        waitUtils.waitForClickable(loginLink).click();
+
+    public LoginPage clickLogin() {
+
+        WebElement loginElement =
+                waitUtils.waitForClickable(loginLink);
+
+        scrollIntoView(loginElement);
+
+        loginElement.click();
 
         return new LoginPage(driver);
     }
-    public SignUpPage clickSignUp(){
-        waitUtils.waitForClickable(signUpLink).click();
+
+    public SignUpPage clickSignUp() {
+
+        WebElement signUpElement =
+                waitUtils.waitForClickable(signUpLink);
+
+        scrollIntoView(signUpElement);
+
+        signUpElement.click();
+
         return new SignUpPage(driver);
     }
-    public ProductPage clickSamsungGalaxyS6(){
-        waitUtils.waitForClickable(samsungGalaxyS6).click();
+
+    public ProductPage clickSamsungGalaxyS6() {
+
+        waitUtils.waitForClickable(
+                samsungGalaxyS6
+        ).click();
+
         return new ProductPage(driver);
     }
 
+    private void scrollIntoView(WebElement element) {
+
+        JavascriptExecutor js =
+                (JavascriptExecutor) driver;
+
+        js.executeScript(
+                "arguments[0].scrollIntoView({block: 'center'});",
+                element
+        );
+    }
 }
+
