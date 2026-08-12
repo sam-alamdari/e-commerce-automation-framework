ef49b10f902833b957c18342ae0458f89bba53d5
elink <msh_372@yahoo.com>
2026-08-12 10:26:23 -0500

    Set consistent browser window size for CI execution

--- src/main/java/com/sam/automation/driver/DriverFactory.java
+++ src/main/java/com/sam/automation/driver/DriverFactory.java
@@ -34,8 +34,14 @@ public class DriverFactory {
                 ChromeOptions options =
                         new ChromeOptions();
 
+                options.addArguments(
+                        "--window-size=1920,1080"
+                );
+
                 if (headless) {
-                    options.addArguments("--headless=new");
+                    options.addArguments(
+                            "--headless=new"
+                    );
                 }
 
                 driver =
