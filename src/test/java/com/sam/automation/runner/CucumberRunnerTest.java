c2b7ac7701c9cb50065c62691d02886c7630a6c6
elink <msh_372@yahoo.com>
2026-08-15 13:49:49 -0500

    Add Cucumber HTML and JSON reporting

--- src/test/java/com/sam/automation/runner/CucumberRunnerTest.java
+++ src/test/java/com/sam/automation/runner/CucumberRunnerTest.java
@@ -7,7 +7,9 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/resources/features",
         glue = "com.sam.automation.steps",
         plugin = {
-                "pretty"
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json"
         }
 )
 public class CucumberRunnerTest
